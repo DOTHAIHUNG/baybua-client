d65a9368662c1ed0fc59de8074e7b2fedea8c79d
HUNGDT <hk2t.thaihung@gmail.com>
2019-02-27 12:17:47 +0700

    updates server.js and config in package.json

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express'),
-  path = require('path');
+//Install express server
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-app.use(express.static('./dist/dummy'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'))
 
-app.get('/*')
+app.get('/*', function (req, res) {
 
+  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'))
+})
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080)
